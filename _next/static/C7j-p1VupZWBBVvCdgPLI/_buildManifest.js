0469749548a2ea0ed20bfb9cc81d41a641e84471
Apollo-111 <110030401+Apollo-111@users.noreply.github.com>
2022-12-21 14:48:46 +0000

    Deploying to public from @ Apollo-111/my-portfolio@d426d2e7126d27e3bc9eff467d2881010fd82e26 🚀

--- _next/static/C7j-p1VupZWBBVvCdgPLI/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/57-2db1494f0ed46dc7.js","static/chunks/pages/index-2afa1d853da63542.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
